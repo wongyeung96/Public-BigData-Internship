54a1a2b03e35b006ae414f4c305f54d63a264123
wongyeung96 <57033367+wongyeung96@users.noreply.github.com>
2021-07-16 17:41:36 +0900

    0707,0708,0714,0715

---
+++ 0707/(4반)김원경_4256/mymodule/myprint2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# !/usr/bin/env python
+# coding: utf-8
+
+def print3(st):
+    print("*" * 50)
+    print(st)
+    print('-' * 50)
+    
+def print4(st):
+    print('-' * 50)
+    print(st)
+    print('*' * 50)
+
