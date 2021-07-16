fb3d2ef1a90946015e69bb26845dcd778835f2ce
wongyeung96 <57033367+wongyeung96@users.noreply.github.com>
2021-07-16 18:13:08 +0900

    SQL파일

---
+++ 0716/0716_연습.sql
@@ -0,0 +1,10 @@
+select * from cslee.tab1;
+select * from cslee.tb_accnt;
+select * from cslee.tb_cust;
+select * from cslee.tb_emp;
+select * from cslee.tb_prod;
+select * from cslee.tb_tran;
+select * from cslee.tb_trcd;
+
+select cust_name, reg_num from cslee.tb_cust;
+
